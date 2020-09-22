1df034c85acab92017d92fcb3411351f905964a7
kmarks2013 <kmarks2013@gmail.com>
2020-09-22 12:54:01 -0400

    sends createstage callback funciton as a prop to Stage component

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { createStage } from '../gameHelpers'
 import Display from './Display'
 import Stage from './Stage'
 import StartButton from './StartButton'
@@ -6,7 +7,7 @@ import StartButton from './StartButton'
 const Tetris = () => {
     return (
         <div>
-            <Stage />
+            <Stage stage={createStage()} />
             <p>I will be tetris</p>
             <aside>
                 <div>
