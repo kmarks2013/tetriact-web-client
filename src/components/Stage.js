896ec0f1eec16dfcd4f72c043465bfb5730edcf9
kmarks2013 <kmarks2013@gmail.com>
2020-09-22 01:06:39 -0400

    adds and renders cell in stage

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Cell from './Cell'
 
 const Stage = ({ stage }) => {
     return (
         <div>
             <p>ill set the stage.</p>
+            <Cell/>
         </div>
     )
 }
