d9bd7704eb6fd16ed2b41fec2c3c473dc4c650af
kmarks2013 <kmarks2013@gmail.com>
2020-09-22 00:55:28 -0400

    adds static render for start button

---
+++ src/components/StartButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StartButton = () => {
+    return (
+        <div>
+            <p>I will be the start button!!!</p>
+        </div>
+    )
+}
+
+export default StartButton
