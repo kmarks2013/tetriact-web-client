0a7181ba5b2193961e8a042bed3ae8415fe4277b
kmarks2013 <kmarks2013@gmail.com>
2020-09-22 00:55:43 -0400

    renders start button in Tetris

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import StartButton from './StartButton'
 
 const Tetris = () => {
     return (
         <div>
             <p>I will be tetris</p>
+            <StartButton/>
         </div>
     )
 }
