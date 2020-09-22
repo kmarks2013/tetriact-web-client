a7ae19650767aae78894d3b76fc4931f75dd28d2
kmarks2013 <kmarks2013@gmail.com>
2020-09-22 13:18:11 -0400

    adds O shape block

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -29,6 +29,12 @@ export const TETROMINOS = {
             [ 0, 'L', 'L', ]
         ],
         color: '223, 173, 36',
-
+    },
+    O: {
+        shape: [
+            [ 'O', 'O' ],
+            [ 'O', 'O' ]
+        ],
+        color: '223, 217, 36',
     },
 }
