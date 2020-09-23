92e03d4d7c04ddcb44e3cb37cb28a6964233c040
kmarks2013 <kmarks2013@gmail.com>
2020-09-23 18:50:39 -0400

    removes starter text and imports styled stage and renders it

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Cell from './Cell'
+import {StyledStage} from './styles/StyledStage'
 
 const Stage = ({ stage }) => {
     return (
-        <div>
-            <p>ill set the stage.</p>
+        <StyledStage width={stage[0].length} height={stage.length}>
             {/* <Cell/> */}
             {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} /> ))}
-        </div>
+        </StyledStage>
     )
 }
 
