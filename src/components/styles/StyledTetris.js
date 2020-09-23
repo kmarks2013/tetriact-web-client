1bf9769fcfacfd35c6ccc78222c494e129eeb92e
kmarks2013 <kmarks2013@gmail.com>
2020-09-23 17:59:59 -0400

    adds Styled tetris wrapper with styling from tutorial

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -1,3 +1,10 @@
 import styled from 'styled-components'
 import bckgImage from 'public/images/bg.png'
 
+export const StyledTetrisWrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    background: url(${bckgImage}, #000);
+    background-size: cover;
+    overflow:hidden;
+`
