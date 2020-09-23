b7575d1fc69dcda082fe5b33b8154b2ca9359204
kmarks2013 <kmarks2013@gmail.com>
2020-09-23 18:10:51 -0400

    updates background color call

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
-import bckgImage from 'public/images/bg.png'
+import bgpng from '../../images/bg.png'
 
 export const StyledTetrisWrapper = styled.div`
     width: 100vw;
     height: 100vh;
-    background: url(${bckgImage}, #000);
+    background: url(${bgpng}), #000;
     background-size: cover;
     overflow:hidden;
 `
