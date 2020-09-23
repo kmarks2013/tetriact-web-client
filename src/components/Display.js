089a48ca859ec4d5b06b42591c10749319dac62c
kmarks2013 <kmarks2013@gmail.com>
2020-09-23 19:02:04 -0400

    imports and renders styled components

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import {StyledDisplay} from './styles/StyleDisplay'
 
 const Display = ({ gameOver, text }) => {
     return (
-        <div>
-            {text}
-        </div>
+        <StyledDisplay gameOver={gameOver}>
+            {text} :
+        </StyledDisplay>
     )
 }
 
