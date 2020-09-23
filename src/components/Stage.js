862d0d2841b49caa91f7f3f45e76403cd7b72c2e
kmarks2013 <kmarks2013@gmail.com>
2020-09-23 19:17:36 -0400

    removes cell  call from stage

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -5,7 +5,6 @@ import {StyledStage} from './styles/StyledStage'
 const Stage = ({ stage }) => {
     return (
         <StyledStage width={stage[0].length} height={stage.length}>
-            {/* <Cell/> */}
             {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} /> ))}
         </StyledStage>
     )
