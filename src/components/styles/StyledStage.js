d31855f712b170189d01d1a72b821e335b5eaaa6
kmarks2013 <kmarks2013@gmail.com>
2020-09-24 13:21:12 -0400

    fixes border error

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -10,7 +10,7 @@ export const StyledStage = styled.div`
         ${props => props.width}, 1fr
     );
     grid-gap: 1px;
-    border: 2xp solid #333;
+    border: 2px solid #333;
     width: 100%;
     max-width: 25vw;
     background: #111;
