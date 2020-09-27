a6f387a0c2e1bb98e06fb2026c86f495ef001ebe
kmarks2013 <kmarks2013@gmail.com>
2020-09-27 18:44:17 -0400

    renders state of positon

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -54,6 +54,7 @@ const Tetris = () => {
 
     return (
         <StyledTetrisWrapper role='button' tabIndex='0' onKeyDown={e => move(e)}>
+         {console.log(player.pos.x, 'state')}
             <StyledTetris>
             <Stage stage={stage} />
             <aside>
