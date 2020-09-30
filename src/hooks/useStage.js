de7fb14f5e0c97052e17e5bacd9a540247e8b5ad
kmarks2013 <kmarks2013@gmail.com>
2020-09-29 23:50:35 -0400

    adds resetPlayer dependency to useeffect

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -34,7 +34,7 @@ export const useStage = (player, resetPlayer) =>{
         }
 
         setStage(prev => updateStage(prev))
-    }, [player])
+    }, [player, resetPlayer])
 
     return [stage, setStage]
 }
