196012cf92436c261f93ec9b828e031b740f51cd
kmarks2013 <kmarks2013@gmail.com>
2020-09-29 21:38:29 -0400

    adds empty useeffect call

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,5 +4,9 @@ import { createStage } from '../gameHelpers'
 export const useStage = (player, resetPlayer) =>{
     const [stage, setStage] = useState(createStage())
 
+    useEffect( () => {
+
+    }, [])
+
     return [stage, setStage]
 }
