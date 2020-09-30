a3ddc9df9046ee785e2d904f8540c2f382921d81
kmarks2013 <kmarks2013@gmail.com>
2020-09-29 21:57:10 -0400

    adds comments and newstage constant that will go thorugh the stage arrays

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -6,7 +6,11 @@ export const useStage = (player, resetPlayer) =>{
 
     useEffect( () => {
         const updateStage = prevStage => {
-
+            //Frst flush the stage
+            //later refacotr for a for loop since it will be faster than a map.
+            const newStage = prevStage.map(row =>
+                row.map( cell => ( cell[1] === 'clear' ? [0,'clear']: cell) )
+                )
         }
 
         setStage(prev => updateStage(prev))
