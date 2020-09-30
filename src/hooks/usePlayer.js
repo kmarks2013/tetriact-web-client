d37021c32dbaad658875c72e3383b35cfd25649f
kmarks2013 <kmarks2013@gmail.com>
2020-09-30 02:59:22 -0400

    adds logic to rotate tetromino against wals and other blocks

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -27,6 +27,18 @@ export const usePlayer = () => {
         const clonedPlayer = JSON.parse(JSON.stringify(player))
         clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir)
 
+        const pos = clonedPlayer.pos.x
+        let offset = 1
+        while(checkCollision(clonedPlayer, stage, {x:0, y:0})){
+            clonedPlayer.pos.x += offset
+            offset = -(offset + (offset> 0 ? 1: -1))
+            if( offset> clonedPlayer.tetromino[0].length){
+                rotate(clonedPlayer.tetromino, -dir)
+                clonedPlayer.pos.x = pos
+                return
+            }
+        }
+
         setPlayer(clonedPlayer)
     }
 
