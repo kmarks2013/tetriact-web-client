e4c01564c481351c90d96b8fd654353315599a64
kmarks2013 <kmarks2013@gmail.com>
2020-10-01 15:27:03 -0400

    remvoes background image from styled component

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 export const StyledTetrisWrapper = styled.div`
     width: 100vw;
     height: 100vh;
-    ${'' /* background: url(${bgpng}), #000; */}
     background-size: cover;
     overflow:hidden;
 `
