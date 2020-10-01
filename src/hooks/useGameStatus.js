5b00fd5b6c132246824a55ac47be596c4d4476f5
kmarks2013 <kmarks2013@gmail.com>
2020-10-01 15:45:28 -0400

    adds linepoints array and calcscore function

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -4,4 +4,10 @@ export const useGameStatus = rowsCleared => {
     const [score, setScore] = useState(0)
     const [rows, setRows] = useState(0)
     const [level, setLevel] = useState(0)
+
+    const linePoints = [40, 100, 300, 1200]
+
+    const calcScore = useCallback(() => {
+        // if we have a score:
+    })
 }
