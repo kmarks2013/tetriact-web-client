6e7700e6b11b314a2ae680199a73482dfff4ebd0
kmarks2013 <kmarks2013@gmail.com>
2020-09-30 22:02:47 -0400

    adds logic to sweep the rows that are completed out

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -8,6 +8,18 @@ export const useStage = (player, resetPlayer) =>{
     useEffect( () => {
         setRowsCleared(0)
 
+        const sweepRows = newStage =>
+            newStage.reduce((ack, row) => {
+                if (row.findIndex(cell => cell[0] === 0) === -1){
+                    setRowsCleared( prev => prev +1 )
+                    //this will replace deleted cells
+                    ack.unshift( new Array(newStage[0].length).fill([0,'clear']))
+                    return ack
+                }
+                ack.push(row)
+                return ack
+            }, [])
+
         const updateStage = prevStage => {
             //Frst flush the stage
             //later refacotr for a for loop since it will be faster than a map.
