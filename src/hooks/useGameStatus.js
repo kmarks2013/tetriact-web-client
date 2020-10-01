d3a069cf3f23e909c4db349e20b3dad263163806
kmarks2013 <kmarks2013@gmail.com>
2020-10-01 15:40:58 -0400

    adds in states for score rows and level

--- src/hooks/useGameStatus.js
+++ src/hooks/useGameStatus.js
@@ -1,5 +1,7 @@
 import {useState, useEffect, useCallback} from 'react'
 
 export const useGameStatus = rowsCleared => {
-
+    const [score, setScore] = useState(0)
+    const [rows, setRows] = useState(0)
+    const [level, setLevel] = useState(0)
 }
