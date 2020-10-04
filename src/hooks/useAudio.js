dfd78ab88224f52ba09f6cf1078e352c7893698a
kmarks2013 <kmarks2013@gmail.com>
2020-10-04 08:22:39 -0400

    adds state and comment about purpose of this file

--- src/hooks/useAudio.js
+++ src/hooks/useAudio.js
@@ -1,4 +1,9 @@
 import {useState, useEffect} from 'react'
 
-export const useAudio =() =>
+export const useAudio = () => {
+    const [audio] = useState(new Audio('https://upload.wikimedia.org/wikipedia/commons/e/e5/Tetris_theme.ogg'));
+    const [playing, setPlaying] = useState(false);
+
+    // this file will set the audio to start playing when a game starts and then restarts teh music when the game starts
+
 };
