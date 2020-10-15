4b9b849cb132e4ee6b856eeb40d52d626cf47ab9
kmarks2013 <kmarks2013@gmail.com>
2020-10-15 16:44:22 -0400

    updates styling to match styling of stage with necessar changes

--- src/components/styles/StyledNextBlock.js
+++ src/components/styles/StyledNextBlock.js
@@ -1,9 +1,23 @@
 import styled from 'styled-components'
 
 export const StyledNextBlock = styled.div`
-    box-sizing: border-box;
-    border: 1px solid black;
+    ${'' /* box-sizing: border-box;
+    border: 1px solid black; */}
     margin: 40px 40px 0 0;
-    height: 100px;
-    width: 100px;
+    ${'' /* height: 100px;
+    width: 100px; */}
+    display: grid;
+    grid-template-rows: repeat(
+        ${props => props.height},
+        calc(10.5vw / ${props => props.width})
+    );
+    grid-template-columns: repeat(
+        ${props => props.width}, 1fr
+    );
+    grid-gap: 2px;
+    border: 4px ridge #333;
+    width: 95px;
+    height: 84px;
+    max-width: 25vw;
+    background-color: #222;
 `
