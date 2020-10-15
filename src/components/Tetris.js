5780f15c51a0d9d2e7aa4a954e7da824d4c58c2d
kmarks2013 <kmarks2013@gmail.com>
2020-10-15 14:51:52 -0400

    adds state of next block and set next block from hook

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -17,9 +17,9 @@ import {createStage, checkCollision, createNext } from '../gameHelpers'
 const Tetris = () => {
     const [ dropTime, setDropTime ] = useState(null)
     const [ gameOver, setGameOver ] = useState(false)
-
     const [ player, updatePlayerPos, resetPlayer, playerRotate ] = usePlayer()
     const [ stage, setStage, rowsCleared ] = useStage(player, resetPlayer)
+    const [ nextBlock, setNextBlock ]= useNextBlock(player, resetPlayer)
     const [ score, setScore, rows, setRows, level, setLevel ] = useGameStatus(rowsCleared)
     const [audio, toggleSound ] = useAudio()
 
