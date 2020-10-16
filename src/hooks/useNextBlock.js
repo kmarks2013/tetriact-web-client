3b9b855728e0d02ed29062a784ff9e7b981ac5bf
kmarks2013 <kmarks2013@gmail.com>
2020-10-16 04:00:19 -0400

    passes player and reset Tetro to useNextBlock

--- src/hooks/useNextBlock.js
+++ src/hooks/useNextBlock.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import {createNext} from '../gameHelpers'
 
 
-export const useNextBlock =  (player, resetPlayer) =>{
+export const useNextBlock =  (player, resetTetro) =>{
     const [nextBlockStage, setNextBlockStage] = useState(createNext()) 
 
     return [nextBlockStage, setNextBlockStage]
