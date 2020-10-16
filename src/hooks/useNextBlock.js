2e25e44eb9aa5ccf91fe1675f35fe7a922f2ddf3
kmarks2013 <kmarks2013@gmail.com>
2020-10-16 04:17:39 -0400

    adds comment about what this hook needs to do next

--- src/hooks/useNextBlock.js
+++ src/hooks/useNextBlock.js
@@ -6,6 +6,7 @@ export const useNextBlock =  (player, resetTetro) =>{
     const [nextBlockStage, setNextBlockStage] = useState(createNext()) 
 
     useEffect( () =>{
+        //this needs to now show the next tetromino block, and then clear it on collision and then add the new one to the next stage
         if (player.collided){
             resetTetro()
             console.log(player)
