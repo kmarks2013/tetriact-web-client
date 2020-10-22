fa3fc5ee10a81bcf9c6a807cf6fd4de480454e53
kmarks2013 <kmarks2013@gmail.com>
2020-10-22 17:55:35 -0400

    comments out console.log in useNextStage

--- src/hooks/useNextStage.js
+++ src/hooks/useNextStage.js
@@ -3,7 +3,7 @@ import { createNext } from '../gameHelpers'
 
 export const useNextStage = ( nextTetro, resetTetro, player) => {
     const [nextStage, setNextStage] = useState(createNext())
-
+    // console.log(player)
     useEffect( () => {
 
         const updateNextStage = prevNextStage =>{
