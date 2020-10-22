244c24106ee75bf923e7e48190cd37e4f1a57f3a
kmarks2013 <kmarks2013@gmail.com>
2020-10-22 17:19:31 -0400

    adds in logic from useStage to map out the nextTetromino

--- src/hooks/useNextStage.js
+++ src/hooks/useNextStage.js
@@ -10,6 +10,17 @@ export const useNextStage = ( nextTetro, resetTetro, player) => {
             const newNextStage = prevNextStage.map(row =>
                 row.map( cell => (cell[1] === 'clear' ?  [0,'clear']: cell ) )
                 )
+            // console.log (newNextStage)
+            nextTetro.tetromino.forEach((row, y) => {
+                // console.log(row, y)
+                row.forEach((value, x) =>{
+                    console.log(nextTetro)
+                    if (value !== 0) {
+                        newNextStage[y + nextTetro.pos.y][x + nextTetro.pos.x] = [value, `${player.collided ? 'merged': 'clear' }`]
+                    }
+                })
+            })
+            return newNextStage
         }
 
     },[])
