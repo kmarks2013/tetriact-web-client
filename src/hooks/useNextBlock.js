edc15ebbb01d5094a1abc90f7cfff74f60206a46
kmarks2013 <kmarks2013@gmail.com>
2020-10-22 14:18:53 -0400

    copies in state from useStage

--- src/hooks/useNextBlock.js
+++ src/hooks/useNextBlock.js
@@ -4,6 +4,8 @@ import {createNext, NEXT_WIDTH} from '../gameHelpers'
 
 export const useNextBlock =  (player, nextTetro, resetTetro) =>{
     const [nextBlockStage, setNextBlockStage] = useState(createNext()) 
+    const [nextRowsCleared, setNextRowsCleared] = useState(0)
+    const {nextTetroPos, setNextTetroPos} = useState({x: 0, y: 0})
 
     useEffect( () =>{
         //this needs to now show the next tetromino block, and then clear it on collision and then add the new one to the next stage
