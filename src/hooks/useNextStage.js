c4522092b0a103b963e2cf211a6a941edbb2fa9c
kmarks2013 <kmarks2013@gmail.com>
2020-10-22 17:11:24 -0400

    adds in space for readabilty in useEffect in useNextStage

--- src/hooks/useNextStage.js
+++ src/hooks/useNextStage.js
@@ -5,6 +5,7 @@ export const useNextStage = ( nextTetro, resetTetro, player) => {
     const [nextStage, setNextStage] = useState(createNext())
 
     useEffect( () => {
+
         const updateNextStage = prevNextStage =>{
             const newNextStage = prevNextStage.map(row =>
                 row.map( cell => (cell[1] === 'clear' ?  [0,'clear']: cell ) )
