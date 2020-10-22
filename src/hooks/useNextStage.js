adc8bfdb84472c89ec89b9b1ab6f63a15607c764
kmarks2013 <kmarks2013@gmail.com>
2020-10-22 16:19:39 -0400

    accepts nextTetro and resetTetro

--- src/hooks/useNextStage.js
+++ src/hooks/useNextStage.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { createNext } from '../gameHelpers'
 
-export const useNextStage = () => {
+export const useNextStage = ( nextTetro, resetTetro) => {
     const [nextStage, setNextStage] = useState(createNext())
 
     useEffect( () => {
