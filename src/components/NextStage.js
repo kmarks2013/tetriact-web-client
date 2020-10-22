9042285392f7d4f717e09ef25861be263045ddfc
kmarks2013 <kmarks2013@gmail.com>
2020-10-22 15:22:56 -0400

    passes nextStage to NextStage

--- src/components/NextStage.js
+++ src/components/NextStage.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Cell from './Cell'
 import {StyledNextBlock} from './styles/StyledNextBlock'
 
-const NextStage = ({ nextBlockStage }) => {
+const NextStage = ({ nextStage }) => {
     return (
         // <StyledNextBlock width={nextBlockStage[0].length} height={nextBlockStage.length}>
         //     {nextBlockStage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} /> ))}
