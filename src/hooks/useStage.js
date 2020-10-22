fc0a3ea1f11a287c6b17468a1a9decf10aeb1123
kmarks2013 <kmarks2013@gmail.com>
2020-10-22 17:10:34 -0400

    removes unnecessary comma from useStage

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -34,8 +34,7 @@ export const useStage = (player, resetPlayer) =>{
                     if (value !== 0) {
                         newStage[y + player.pos.y][x+ player.pos.x] = [
                             value,
-                            `${player.collided ? 'merged': 'clear'}`,
-
+                            `${player.collided ? 'merged': 'clear'}`
                         ]
                     }
                 })
