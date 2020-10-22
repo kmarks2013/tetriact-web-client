a731b48101b2bb765718552a27699f4f0094e040
kmarks2013 <kmarks2013@gmail.com>
2020-10-22 15:43:13 -0400

    deletes useNextBlock hook

--- src/hooks/useNextBlock.js
+++
@@ -1,7 +0,0 @@
-import { useState, useCallback } from 'react'
-import { TETROMINO, randomTetromino } from '../tetrominos'
-import { checkCollisson, NEXT_WIDTH } from '../gameHelpers'
-
-export const useNextBlock = () => {
-
-}
