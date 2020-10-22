1380dc2a7ab61864f0b16312c4e79c280d50ee3a
kmarks2013 <kmarks2013@gmail.com>
2020-10-22 14:43:51 -0400

    changes name of nextBlock file to Next stage

---
+++ src/components/NextStage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Cell from './Cell'
+import {StyledNextBlock} from './styles/StyledNextBlock'
+
+const NextStage = ({ nextBlockStage }) => {
+    return (
+        // <StyledNextBlock width={nextBlockStage[0].length} height={nextBlockStage.length}>
+        //     {nextBlockStage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} /> ))}
+        // </StyledNextBlock>
+        <div> I will be the next stage</div>
+    )
+}
+
+export default NextStage
