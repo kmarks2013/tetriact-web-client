08d4ffdfd6f6c6f37712ea3aaa0b1f55284c690f
kmarks2013 <kmarks2013@gmail.com>
2020-10-23 00:08:33 -0400

    removes h1 title from app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Footer from './components/Footer'
 function App() {
   return (
     <div className="App">
-        <h1 id='title'>TETRIACT</h1>
         <Tetris/>
         <Footer />
     </div>
