2121b57e08b29bf7882f6d5b39810d20c4eb7e0e
kmarks2013 <kmarks2013@gmail.com>
2020-10-23 00:22:34 -0400

    changes size of styled tetris wrapper

--- src/components/styles/StyledTetris.js
+++ src/components/styles/StyledTetris.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const StyledTetrisWrapper = styled.div`
     width: 100vw;
-    height: 90vh;
+    height: 80vh;
     background-size: cover;
     overflow:hidden;
 `
