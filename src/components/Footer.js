f6fa287d69a31c2e7bdc606e7fb53b2efc1ce54b
kmarks2013 <kmarks2013@gmail.com>
2020-10-22 23:56:54 -0400

    adds a tag to create attribution to free pik

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div>
+            <a href='https://www.freepik.com/vectors/background'>Background vector created by freepik - www.freepik.com</a>
         </div>
     )
 }
