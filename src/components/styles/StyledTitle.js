3be36a496de428f09c74696b6e4619bb4c5038ed
kmarks2013 <kmarks2013@gmail.com>
2020-10-22 23:54:57 -0400

    adds styling to styled title

--- src/components/styles/StyledTitle.js
+++ src/components/styles/StyledTitle.js
@@ -1,5 +1,18 @@
 import styled from 'styled-components'
 
 export const StyledTitle = styled.div`
+    display:flex;
+    width: 100vw;
+    height: 10vh;
+    align-items: center;
+
+    h1 {
+        font-family: johnny fever, Arial, Helvetica, sans-serif;
+        font-style: italic;
+        font-size: 100px;
+        margin: auto;
+        height: 10vh;
+        color: rgb(255,161,61) ;
+    }
 
 `
