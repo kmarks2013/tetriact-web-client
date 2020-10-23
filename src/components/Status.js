6a50341feb44ffb58618fae428b392e4714a45ac
kmarks2013 <kmarks2013@gmail.com>
2020-10-23 18:41:00 -0400

    adds basic render to status screen

---
+++ src/components/Status.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Status = () => {
+    return (
+        <div>
+            <p> I will be pause or game over screen</p>
+        </div>
+    )
+}
+
+export default Status
