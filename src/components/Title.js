9e2311ffcbd667fa30f1dcedae716852e92ef151
kmarks2013 <kmarks2013@gmail.com>
2020-10-23 00:06:44 -0400

    adds styled title to title

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyledTitle } from './styles/StyledTitle'
+
+function Title() {
+    return (
+        <StyledTitle>
+            <h1>TETRIACT</h1>
+        </StyledTitle>
+    )
+}
+
+export default Title
