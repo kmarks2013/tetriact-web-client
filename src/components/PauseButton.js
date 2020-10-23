1b2284216b0a9c2cd236790f23e2c0f0ed9d3d08
kmarks2013 <kmarks2013@gmail.com>
2020-10-23 18:40:43 -0400

    adds basic render to pause button

---
+++ src/components/PauseButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PauseButton = () => {
+    return (
+        <div>
+            <p> I will be the pause button and styled similarl to hte start button</p>
+        </div>
+    )
+}
+
+export default PauseButton
