9f279200aca1a2244fe431b01cd299d50793678f
kmarks2013 <kmarks2013@gmail.com>
2020-10-26 19:30:37 -0400

    adds a PauseMenuRapper  with styling

---
+++ src/components/styles/StyledPauseMenu.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const StyledPauseMenuWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba( 118, 114, 138, 0.3 );
+    display: flex;
+`
