76ccce7f4838b11d1222f7c08fd712d8d2e6bcf2
kmarks2013 <kmarks2013@gmail.com>
2020-10-26 17:42:02 -0400

    adds styled import and borrows styling from styledbutton

---
+++ src/components/styles/StyledAudioMenu.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const StyledAudioMenu = styled.div`
+    display:flex;
+    flex-direction: flex-wrap;
+    box-sizing: border-box;
+    margin: 0 0 13p 0;
+    padding: 20px;
+    min-height: 30px;
+    width: 100%;
+    border-radius: 15px;
+    border: none;
+    background: #333;
+    outline: none;
+    cursor: pointer;
+`
