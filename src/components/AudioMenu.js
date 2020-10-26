63e15da7d7ea23e69cb4d66f6680485a47a34ddc
kmarks2013 <kmarks2013@gmail.com>
2020-10-26 17:39:22 -0400

    adds imports for font awesome icons

--- src/components/AudioMenu.js
+++ src/components/AudioMenu.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { faPlay, faPause, faVolumeUp, faVolumeMute, faVolumeDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const AudioMenu = () => {
     return (
