11b4fe728a1a43b644c4bf5559922eae7e513a48
kmarks2013 <kmarks2013@gmail.com>
2020-10-26 16:02:27 -0400

    updates button callback to be callback from props

--- src/components/PauseMenu.js
+++ src/components/PauseMenu.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import Button from './Button'
 
-const PauseMenu = () => {
+const PauseMenu = ({callback}) => {
     return (
         <div>
             Game Paused
-            <Button text='Resume Game' callback={() => console.log('resumeGame')} />
+            <Button text='Resume Game' callback={callback} />
         </div>
     )
 }
