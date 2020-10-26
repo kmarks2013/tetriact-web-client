8c020dabb712cc785ed115514e732ef68a79d2c5
kmarks2013 <kmarks2013@gmail.com>
2020-10-26 17:48:59 -0400

    adds styled icon from styled icon

--- src/components/AudioMenu.js
+++ src/components/AudioMenu.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { faPlay, faPause, faVolumeUp, faVolumeMute, faVolumeDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { StyledAudioMenu } from './styles/StyledAudioMenu'
+import {StyledIcon} from './styles/StyledIcon'
 
 const AudioMenu = () => {
     return (
