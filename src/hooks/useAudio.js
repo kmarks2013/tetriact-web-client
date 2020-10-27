cb069916e32fae722f8cb948c0a82f468b38f611
kmarks2013 <kmarks2013@gmail.com>
2020-10-26 22:19:03 -0400

    removes playing state from useAudio

--- src/hooks/useAudio.js
+++ src/hooks/useAudio.js
@@ -2,7 +2,6 @@ import {useState, useEffect} from 'react'
 
 export const useAudio = () => {
     const [audio] = useState(new Audio('https://upload.wikimedia.org/wikipedia/commons/e/e5/Tetris_theme.ogg'));
-    const [playing, setPlaying] = useState(false);
 
     // this file will set the audio to start playing when a game starts and then restarts teh music when the game starts
     // console.log(audio.loop)
