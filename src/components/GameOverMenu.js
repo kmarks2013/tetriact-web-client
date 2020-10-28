7be849b4bdc8827c7030b4f3575eb1e7496e810e
kmarks2013 <kmarks2013@gmail.com>
2020-10-28 18:08:02 -0400

    updates basic render for gameover screen

--- src/components/GameOverMenu.js
+++ src/components/GameOverMenu.js
@@ -3,9 +3,29 @@ import { StyledPopUpWrapper, StyledGameOverMenu } from './styles/StyledPopUps'
 
 const GameOverMenu = () => {
     return (
-        <div>
-            this will be the gameover menu
-        </div>
+        <StyledPopUpWrapper>
+            <StyledGameOverMenu>
+
+                <p>
+                this will be the gameover menu
+                </p>
+                <p>
+                To see if you scored in the top 10 enter a name below:
+                </p>
+                <p>
+                This will then be a form.
+                </p>
+                <p>
+                the form once complete will then take you to the high scores screen.
+                </p>
+                <p>
+
+                This screen will also allow a player to restart from scratch, or close their window so they can look at their game.
+                </p>
+
+            </StyledGameOverMenu>
+        </StyledPopUpWrapper>
+
     )
 }
 
