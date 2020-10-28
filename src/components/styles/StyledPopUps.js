993223da9737899975ae76fb519b7e0ee62f9eb0
kmarks2013 <kmarks2013@gmail.com>
2020-10-28 18:02:43 -0400

    renames styled pause menu to styled pop up

---
+++ src/components/styles/StyledPopUps.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const StyledPopUpWrapper = styled.div`
+    position: absolute;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba( 118, 114, 138, 0.3 );
+    display: flex;
+`
+
+export const StyledPauseMenu = styled.div`
+    width: 50%;
+    height: 50%;
+    margin: auto;
+    background: #03427f;
+`
+export const StyledGameOverMenu = styled.div`
+    width: 50%;
+    height: 50%;
+    margin: auto;
+    background: #03427f;
+`
