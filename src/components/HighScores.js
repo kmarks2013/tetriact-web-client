08d883791497d035ba592b2eabf152de10afe7fc
kmarks2013 <kmarks2013@gmail.com>
2020-10-29 18:24:49 -0400

    ads basic render for high scores

---
+++ src/components/HighScores.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HighScores = () => {
+    return (
+        <div>
+            Adds a basic render for the high scores
+        </div>
+    )
+}
+
+export default HighScores
