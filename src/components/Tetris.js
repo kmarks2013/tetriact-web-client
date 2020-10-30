ebdbab5311f8668bc26b8970c33eaf74883770a5
kmarks2013 <kmarks2013@gmail.com>
2020-10-30 14:31:16 -0400

    passes score to gameover menu

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -161,7 +161,7 @@ const Tetris = () => {
                 <AudioMenu song={audio} />
             </aside>
             { paused ?  <PauseMenu callback={resumeGame}/> : null}
-            { gameOver ?  <GameOverMenu newGame={startGame} closeMenu={closeGameOver} clearBoard={clearBoard}/> : null }
+            { gameOver ?  <GameOverMenu newGame={startGame} closeMenu={closeGameOver} clearBoard={clearBoard} score={score}/> : null }
             { scoreBoard ? <HighScores /> : null}
             </StyledTetris>
             <Footer />
