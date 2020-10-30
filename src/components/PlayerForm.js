3f73eb0904bed69be555fb6ca04c8d92176d26e8
kmarks2013 <kmarks2013@gmail.com>
2020-10-30 17:31:50 -0400

    adds console.log of newgame to console.log

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -5,6 +5,7 @@ import { StyledGameOverMenu } from './styles/StyledPopUps'
 export const PlayerForm = ({score, newGame}) => {
     return (
         <div>
+            {console.log(score, newGame)}
             <h2> Game Over... </h2>
             <h3> Your Score Was: a score </h3>
             <h4> To save your score enter a name below:</h4>
