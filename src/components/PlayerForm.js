61b46e04a1c989a0bd9106d2f2cd049f5865a052
kmarks2013 <kmarks2013@gmail.com>
2020-10-30 17:21:59 -0400

    renames form as playerform

---
+++ src/components/PlayerForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyledGameOverMenu } from './styles/StyledPopUps'
+
+
+export const PlayerForm = () => {
+    return (
+        <div>
+            <h2> Game Over... </h2>
+            <h3> Your Score Was: a score </h3>
+        </div>
+    )
+}
