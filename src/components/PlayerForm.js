fb944a7afbf61765ae34353f7ef5f7be2cf1357a
kmarks2013 <kmarks2013@gmail.com>
2020-10-30 18:32:31 -0400

    refactors onsubmit to just run handlesubmit

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -15,7 +15,7 @@ export const PlayerForm = ({score, newGame, handleSubmit, setGamerTag, gamerTag}
             <h2> Game Over... </h2>
             <h3> Your Score Was: {score} </h3>
             <h4> To save your score enter a name below:</h4>
-            <form onSubmit={(e) => handleSubmit(e, gamerTag)}>
+            <form onSubmit={handleSubmit}>
                 <input onChange={handleChange} type='text' vlaue={gamerTag} />
                 <input type='submit' />
             </form>
