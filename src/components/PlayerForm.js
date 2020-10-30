814496a97dc6001b0b18839f9d26847974c52e77
kmarks2013 <kmarks2013@gmail.com>
2020-10-30 17:50:40 -0400

    renders score in h3 tag

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -9,7 +9,7 @@ export const PlayerForm = ({score, newGame}) => {
         <StyledGameOverMenu>
             {console.log(gamerTag)}
             <h2> Game Over... </h2>
-            <h3> Your Score Was: a score </h3>
+            <h3> Your Score Was: {score} </h3>
             <h4> To save your score enter a name below:</h4>
             <form>
                 <input type='text' />
