0103b08fa18f6face9132086f5ab3c2e2822a2d0
kmarks2013 <kmarks2013@gmail.com>
2020-10-30 17:58:59 -0400

    fixes handleChange spelling error

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -16,7 +16,7 @@ export const PlayerForm = ({score, newGame}) => {
             <h3> Your Score Was: {score} </h3>
             <h4> To save your score enter a name below:</h4>
             <form>
-                <input onChange={hanldeChange} type='text' vlaue={gamerTag} />
+                <input onChange={handleChange} type='text' vlaue={gamerTag} />
                 <input type='submit' />
             </form>
             <Button text='Play Again?' callback={newGame} />
