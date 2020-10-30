a3955e50b939d278a38ddb2ffd560966659a0e7b
kmarks2013 <kmarks2013@gmail.com>
2020-10-30 17:24:55 -0400

    adds a form with text input and submit button

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -8,6 +8,10 @@ export const PlayerForm = () => {
             <h2> Game Over... </h2>
             <h3> Your Score Was: a score </h3>
             <h4> To save your score enter a name below:</h4>
+            <form>
+                <input type='text' />
+                <input type='submit' />
+            </form>
         </div>
     )
 }
