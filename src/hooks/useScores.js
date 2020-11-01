714a1ddcac64c111ea0ad492d4374c3aa3fba314
kmarks2013 <kmarks2013@gmail.com>
2020-11-01 11:40:14 -0500

    trys lower score tosee if we get 11 in console.log for topScores

--- src/hooks/useScores.js
+++ src/hooks/useScores.js
@@ -40,7 +40,7 @@ export const useScores = (gamePlayer) => {
     // topScores()
 
     const saveScore = (gamerTag, playerScore) => {
-        const formData = { gamertag: gamerTag, score: 50000}
+        const formData = { gamertag: gamerTag, score: 7000}
         console.log(formData)
         //this will post the score to user with params of gamertag and score.
         // it will then update the state of scores and console.log it.
