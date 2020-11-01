f70e02185704d1ff664a960b762d9aad55d48c2d
kmarks2013 <kmarks2013@gmail.com>
2020-11-01 09:25:52 -0500

    copies component initalization line and renders just the player and gets scores and topTen from useScores

--- src/components/HighScores.js
+++ src/components/HighScores.js
@@ -3,8 +3,9 @@ import { StyledPopUpWrapper, StyledHighScores } from './styles/StyledPopUps'
 import { useScores } from '../hooks/useScores'
 
 
-const HighScores = ({player, scores, topTen}) => {
-    // const {scores, topTen} = useScores()
+// const HighScores = ({player, scores, topTen}) => {
+const HighScores = ({player}) => {
+    const {scores, topTen} = useScores()
 
     // i will need to lsit out the top ten socres... may make sense to get the top ten from useScores instead...
     let i = 1
