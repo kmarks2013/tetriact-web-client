5c3ef55d4315ca73f4dad67271f1168f83dd6db7
kmarks2013 <kmarks2013@gmail.com>
2020-11-06 14:20:22 -0500

    adds imports for react tetris into APP

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import Tetris from './components/Tetris';
+import { transitions, positions, Provider as AlertProvider } from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 
 function App() {
   return (
