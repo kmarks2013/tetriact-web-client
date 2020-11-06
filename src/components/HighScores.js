cc7075a60b0414f995b5742a5e76d1181b477df9
kmarks2013 <kmarks2013@gmail.com>
2020-11-06 12:47:36 -0500

    removes old p tag

--- src/components/HighScores.js
+++ src/components/HighScores.js
@@ -57,7 +57,6 @@ return (
                                             </>
                                         )
                                     })}
-                            {/* <p>hi</p> */}
                                     </tbody>
                                 </table>
                             </>
