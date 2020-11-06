cc1c7373477e29c733b5b4b48adfea9460be0f03
kmarks2013 <kmarks2013@gmail.com>
2020-11-06 13:30:56 -0500

    adds background image p tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
     return (
         <StyledFooter>
             <p> Attributions: </p>
+            <p> Background Image: </p>
             <a href='https://www.freepik.com/vectors/background'> Background vector created by freepik - www.freepik.com </a>
             <a href="https://commons.wikimedia.org/wiki/File:Tetris_theme.ogg"> Bogozi </a>
             <a href="https://creativecommons.org/licenses/by-sa/3.0"> CC BY-SA 3.0 , via Wikimedia Commons </a>
