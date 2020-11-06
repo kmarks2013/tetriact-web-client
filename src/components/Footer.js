4b91d4585b7e0aee26998208b83c4feb08026895
kmarks2013 <kmarks2013@gmail.com>
2020-11-06 13:31:13 -0500

    adds muscal score p tag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,7 @@ const Footer = () => {
             <p> Attributions: </p>
             <p> Background Image: </p>
             <a href='https://www.freepik.com/vectors/background'> Background vector created by freepik - www.freepik.com </a>
+            <p> Musical Score: </p>
             <a href="https://commons.wikimedia.org/wiki/File:Tetris_theme.ogg"> Bogozi </a>
             <a href="https://creativecommons.org/licenses/by-sa/3.0"> CC BY-SA 3.0 , via Wikimedia Commons </a>
         </StyledFooter>
