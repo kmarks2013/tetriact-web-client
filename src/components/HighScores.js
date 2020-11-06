cec1409cdb4aa52640359137d3b4b1e67de0f3e6
kmarks2013 <kmarks2013@gmail.com>
2020-11-06 12:55:04 -0500

    updates spacing again highScores

--- src/components/HighScores.js
+++ src/components/HighScores.js
@@ -12,85 +12,76 @@ const HighScores = ({player}) => {
 
 
     const playerIndex = scores.findIndex(score => score.id === player.id) + 1
-    // const renderScores = () => {
-        //     let newScores = []
-    //     topTen
-    //     if (topTen.includes(player)){
-    //         console.log('hi')
-    //     } else{
-        //         console.log('bye')
-        //     }
-        // }
+
         
 
-return (
-    <StyledHighScores>
-
-                <h2>
-                    High Scores:
-                </h2>
-                { scores.length && player ?
-                    <div>
-                        {gameScores().length ?
-                            <>
-                                <table>
-                                    <tbody>
-                                        {gameScores().map( score => {
-                                            return(
-                                                <>
-                                                    {score.id === player.id?
-                                                        <tr style={{color: 'red'}} key={player.id}>
-                                                            <td>{i++}</td>
-                                                            <td>{player.gamertag}</td>
-                                                            <td>{player.score}</td>
-                                                        {/* <p style={{color: 'red'}}>{player.gamertag}</p> */}
-                                                        </tr>
-                                                    :
-                                                        <tr style={{color: 'white'}} key={player.id}>
-                                                            <td>{i++}</td>
-                                                            <td>{score.gamertag}</td>
-                                                            <td>{score.score}</td>
-                                                        {/* <p style={{color: 'white'}}> {score.score} </p> */}
-                                                        </tr>
-                                                    }
-                                                </>
-                                            )
-                                        })}
-                                    </tbody>
-                                </table>
-                            </>
-                        :
-                            <>
-                                <table>
-                                    <tbody>
-                                       {topScores().map( top => {
-                                            return(
-                                                <>
-                                                    {top.id === player.id ?
-                                                        <tr style={{color: 'red'}}>
-                                                            <td>{playerIndex}</td>
-                                                            <td>{player.gamertag}</td>
-                                                            <td>{player.score}</td>
-                                                        </tr>
-                                                    :
-                                                        <tr style={{color: 'white'}}>
-                                                            <td>{i++}</td>
-                                                            <td>{top.gamertag}</td>
-                                                            <td>{top.score}</td>
-                                                        </tr>
-                                                    }
-                                                </>
-                                            )
-                                        })}
-                                    </tbody>
-                                </table>
-                            </>
-                        }
-                    </div>
-                :
-                <h3> Loading Scores...</h3>
-                }
-            </StyledHighScores>
+    return (
+        <StyledHighScores>
+            <h2>
+                High Scores:
+            </h2>
+            { scores.length && player ?
+                <div>
+                    {gameScores().length ?
+                        <>
+                            <table>
+                                <tbody>
+                                    {gameScores().map( score => {
+                                        return(
+                                            <>
+                                                {score.id === player.id?
+                                                    <tr style={{color: 'red'}} key={player.id}>
+                                                        <td>{i++}</td>
+                                                        <td>{player.gamertag}</td>
+                                                        <td>{player.score}</td>
+                                                    {/* <p style={{color: 'red'}}>{player.gamertag}</p> */}
+                                                    </tr>
+                                                :
+                                                    <tr style={{color: 'white'}} key={player.id}>
+                                                        <td>{i++}</td>
+                                                        <td>{score.gamertag}</td>
+                                                        <td>{score.score}</td>
+                                                    {/* <p style={{color: 'white'}}> {score.score} </p> */}
+                                                    </tr>
+                                                }
+                                            </>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </>
+                    :
+                        <>
+                            <table>
+                                <tbody>
+                                {topScores().map( top => {
+                                        return(
+                                            <>
+                                                {top.id === player.id ?
+                                                    <tr style={{color: 'red'}}>
+                                                        <td>{playerIndex}</td>
+                                                        <td>{player.gamertag}</td>
+                                                        <td>{player.score}</td>
+                                                    </tr>
+                                                :
+                                                    <tr style={{color: 'white'}}>
+                                                        <td>{i++}</td>
+                                                        <td>{top.gamertag}</td>
+                                                        <td>{top.score}</td>
+                                                    </tr>
+                                                }
+                                            </>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </>
+                    }
+                </div>
+            :
+            <h3> Loading Scores...</h3>
+            }
+        </StyledHighScores>
     )
 }
 
