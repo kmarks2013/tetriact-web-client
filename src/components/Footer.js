c464ed84684e57c8ddfae8d15a32b56e836adc0f
kmarks2013 <kmarks2013@gmail.com>
2020-11-06 13:29:14 -0500

    adds links for tetris theme attribution

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,9 @@ import { StyledFooter } from './styles/StyledFooter'
 const Footer = () => {
     return (
         <StyledFooter>
-            <a href='https://www.freepik.com/vectors/background'>Background vector created by freepik - www.freepik.com</a>
+            <a href='https://www.freepik.com/vectors/background'> Background vector created by freepik - www.freepik.com </a>
+            <a href="https://commons.wikimedia.org/wiki/File:Tetris_theme.ogg"> Bogozi </a>
+            <a href="https://creativecommons.org/licenses/by-sa/3.0"> CC BY-SA 3.0 , via Wikimedia Commons </a>
         </StyledFooter>
     )
 }
