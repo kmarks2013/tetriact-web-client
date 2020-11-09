5814a5bf92707b1f5cbc2844f2bbd80753b90669
kmarks2013 <kmarks2013@gmail.com>
2020-11-09 15:09:26 -0500

    updates prop calls to remove the word cell

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -4,11 +4,11 @@ export const StyledStage = styled.div`
     margin: 0 0 0 40px;
     display: grid;
     grid-template-rows: repeat(
-        ${props => props.cellHeight},
-        calc(${props => props.vw}vw / ${props => props.cellWidth})
+        ${props => props.height},
+        calc(${props => props.vw}vw / ${props => props.width})
     );
     grid-template-columns: repeat(
-        ${props => props.cellWidth}, 1fr
+        ${props => props.width}, 1fr
     );
     grid-gap: 2px;
     border: 4px ridge #333;
