a911e0b06d27af00a2e35222ae84e74066410f2b
kmarks2013 <kmarks2013@gmail.com>
2020-11-09 14:44:04 -0500

    updates height and width to cellheight and cellwidt and vw

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -4,8 +4,8 @@ export const StyledStage = styled.div`
     margin: 0 0 0 40px;
     display: grid;
     grid-template-rows: repeat(
-        ${props => props.height},
-        calc(25vw / ${props => props.width})
+        ${props => props.cellHeight},
+        calc(${props => props.vw}vw / ${props => props.cellWidth})
     );
     grid-template-columns: repeat(
         ${props => props.width}, 1fr
