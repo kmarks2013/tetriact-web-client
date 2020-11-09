25455e2441c309d8193b15eaff460e2ec8a5ec7c
kmarks2013 <kmarks2013@gmail.com>
2020-11-09 14:39:53 -0500

    updates Styled Stage props to match reusable components

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -5,7 +5,7 @@ import { STAGE_VW } from '../gameHelpers'
 
 const Stage = ({ stage }) => {
     return (
-        <StyledStage width={stage[0].length} height={stage.length}>
+        <StyledStage cellWidth={stage[0].length} vw={STAGE_VW} cellHeight={stage.length}>
             {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} /> ))}
         </StyledStage>
     )
