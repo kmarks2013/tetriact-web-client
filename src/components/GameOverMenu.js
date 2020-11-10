d55e55a87d6b4bff47518fa54a7eba15e277838b
kmarks2013 <kmarks2013@gmail.com>
2020-11-10 00:07:00 -0500

    removes button from gameover menu

--- src/components/GameOverMenu.js
+++ src/components/GameOverMenu.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import Button from './Button'
 import { StyledPopUpWrapper, StyledCloseButton } from './styles/StyledPopUps'
 
 import { useScores } from '../hooks/useScores'
