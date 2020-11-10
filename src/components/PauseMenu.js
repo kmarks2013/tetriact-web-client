9e27547b010a72e518c267c9744dd819cb821d39
kmarks2013 <kmarks2013@gmail.com>
2020-11-09 23:32:22 -0500

    adds spacing to h2 and passes margin to button

--- src/components/PauseMenu.js
+++ src/components/PauseMenu.js
@@ -6,8 +6,8 @@ const PauseMenu = ({callback}) => {
     return (
         <StyledPopUpWrapper>
             <StyledPauseMenu >
-                <h2>Game Paused</h2>
-                <Button text='Resume Game' callback={callback} />
+                <h2> Game Paused </h2>
+                <Button text='Resume Game' width={45} margin={120} callback={callback} />
             </StyledPauseMenu>
         </StyledPopUpWrapper>
     )
