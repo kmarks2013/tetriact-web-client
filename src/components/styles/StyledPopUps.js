9cc577b1a6e3443648c6b8b685fe5c5f5ee91bd1
kmarks2013 <kmarks2013@gmail.com>
2020-11-09 20:47:25 -0500

    adds in props to button

--- src/components/styles/StyledPopUps.js
+++ src/components/styles/StyledPopUps.js
@@ -113,7 +113,7 @@ export const StyledHighScores = styled.div`
 export const StyledCloseButton = styled.button`
     box-sizing: border-box;
     ${'' /* min-height: 30px; */}
-    margin: 0px 0px 0px 550px;
+    margin: 0px 0px 0px ${props => props.margin}px;
     width: 5%;
     height: 5%;
     border-radius: 15px;
