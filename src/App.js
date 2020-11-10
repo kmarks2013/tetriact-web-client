aeca1f11d98b3d3eed08a5908ab95cc30283c10f
kmarks2013 <kmarks2013@gmail.com>
2020-11-09 23:37:01 -0500

    removes alert template import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Tetris from './components/Tetris';
 import { transitions, positions, Provider as AlertProvider } from 'react-alert'
-import AlertTemplate from 'react-alert-template-basic'
 
 function App() {
   return (
