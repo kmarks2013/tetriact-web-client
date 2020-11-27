8742754743a90950f86b6368becb89d00ce4a434
kmarks2013 <kmarks2013@gmail.com>
2020-11-27 17:07:00 -0500

    changes h3 text

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -6,7 +6,7 @@ import { StyledInstructions } from './styles/StyledInstructions';
 export const Instructions = () => {
     return (
         <StyledInstructions>
-            <h3>Gameplay Instructions</h3>
+            <h3>How To Play:</h3>
             <p>Rotate Tetromino: Up Arrow</p>
             <p>Move Tetromino Left: Left Arrow</p>
             <p>Move Tetromino Right: Right Arrow</p>
