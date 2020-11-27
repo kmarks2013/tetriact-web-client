e09c934b9b3320e6873568d30d48c5811be3bcd2
kmarks2013 <kmarks2013@gmail.com>
2020-11-27 15:59:25 -0500

    add moves tetromino left instruction

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 export const Instructions = () => {
     return (
         <div>
             <h3>Gameplay Instructions</h3>
             <p>Rotate Tetromino: Up Arrow</p>
+            <p>Move Tetromino Left: Left Arrow</p>
         </div>
     )
 }
