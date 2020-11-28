7c10ddd91155133a54613cb064a9c8d274ceb1e1
kmarks2013 <kmarks2013@gmail.com>
2020-11-27 23:57:02 -0500

    removes margin from close button

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -10,7 +10,7 @@ export const PlayerForm = ({score, newGame, handleSubmit, setGamerTag, gamerTag,
 
     return (
         <StyledGameOverMenu>
-            <StyledCloseButton margin={550} onClick={closeMenu}>
+            <StyledCloseButton onClick={closeMenu}>
                     X
             </StyledCloseButton>
             <h2> Game Over... </h2>
