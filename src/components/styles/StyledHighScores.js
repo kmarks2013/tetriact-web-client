7ff1535421e2430fa5ad8b919e3e9d6a9c509464
kmarks2013 <kmarks2013@gmail.com>
2020-11-28 00:44:31 -0500

    adds flex and flex direction to styled high scores

--- src/components/styles/StyledHighScores.js
+++ src/components/styles/StyledHighScores.js
@@ -1,6 +1,8 @@
 import styled from 'styled-components'
 
 export const StyledHighScores = styled.div`
+    display: flex;
+    flex-direction: column;
     width: 50%;
     margin: auto;
     height: auto;
