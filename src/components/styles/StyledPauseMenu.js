818e4be31fdc88f28f05e22066301c4da4f21a4b
kmarks2013 <kmarks2013@gmail.com>
2020-11-27 19:55:15 -0500

    copies in styledPausemenu from popups

--- src/components/styles/StyledPauseMenu.js
+++ src/components/styles/StyledPauseMenu.js
@@ -1,2 +1,19 @@
 import styled from 'styled-components'
 
+export const StyledPauseMenu = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: auto;
+    height: auto;
+    margin: auto;
+    background: #000;
+    opacity: 0.8;
+
+    h2 {
+        font-family: Pixel, Arial, Helvetica, sans-serif;
+        font-size: 50px;
+        margin: auto;
+        padding: 40px;
+        color: white ;
+    }
+`
