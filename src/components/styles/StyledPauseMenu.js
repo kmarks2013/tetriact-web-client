8fa2d86f4698c83f349fe596691b1e53523a5343
kmarks2013 <kmarks2013@gmail.com>
2020-11-27 20:46:09 -0500

    copies in button styling with a few changes

--- src/components/styles/StyledPauseMenu.js
+++ src/components/styles/StyledPauseMenu.js
@@ -17,3 +17,20 @@ export const StyledPauseMenu = styled.div`
         color: white ;
     }
 `
+
+export const StyledUnpauseButton = styled.button`
+        box-sizing: border-box;
+        margin: auto ;
+        padding: 20px;
+        min-height: 30px;
+        width: 45%;
+        height: auto;
+        border-radius: 15px;
+        border: none;
+        color: white;
+        background: #333;
+        font-family: Pixel, Arial, Helvetica, sans-serif;
+        font-size: 1rem;
+        outline: none;
+        cursor: pointer;
+`
