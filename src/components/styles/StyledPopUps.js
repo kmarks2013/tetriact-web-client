771aea03ab59251cbeecf1d611db73f1e3da0ad1
kmarks2013 <kmarks2013@gmail.com>
2020-11-28 00:40:40 -0500

    deletes gameover menu styling

--- src/components/styles/StyledPopUps.js
+++ src/components/styles/StyledPopUps.js
@@ -9,53 +9,6 @@ export const StyledPopUpWrapper = styled.div`
     display: flex;
     z-index: 1;
 `
-export const StyledGameOverMenu = styled.div`
-    width: auto;
-    height: auto;
-    margin: auto;
-    background: #03427f;
-    color: white;
-    background: #000;
-    opacity: 0.8;
-
-    h2 {
-        font-family: Pixel, Arial, Helvetica, sans-serif;
-        font-size: 50px;
-        margin: auto;
-        padding: 20px;
-        color: red ;
-    }
-
-    h3 {
-        font-family: Pixel, Arial, Helvetica, sans-serif;
-        font-size: 24px;
-        margin: auto;
-        ${'' /* padding: 20px; */}
-        color: white ;
-    }
-
-    h4 {
-        font-family: Pixel, Arial, Helvetica, sans-serif;
-        font-size: 20px;
-        margin: auto;
-        padding: 20px;
-        color: white ;
-    }
-
-    form {
-        padding-bottom: 15px ;
-    }
-
-    label, input {
-        font-family: Pixel, Arial, Helvetica, sans-serif;
-        padding: 0px 10px 0px 10px;
-        margin: auto;
-    }
-
-    input {
-        font-family: Arial, Helvetica, sans-serif;
-    }
-`
 
 export const StyledHighScores = styled.div`
     width: 50%;
