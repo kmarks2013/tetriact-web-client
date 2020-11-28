f5b8a22b3dd427cb719d90beaa33c6bf9412db84
kmarks2013 <kmarks2013@gmail.com>
2020-11-27 19:10:55 -0500

    adds styling to styled side bar

--- src/components/styles/StyledSideBar.js
+++ src/components/styles/StyledSideBar.js
@@ -1,5 +1,8 @@
 import styled from 'styled-components'
 
 export const StyledSideBar = styled.div`
-
+    display:flex;
+    flex-direction: column;
+    align-items: flex-end;
+    margin-right: 2%;
 `
