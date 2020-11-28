9ab8291035b9da27a4f8a611572d0029859e80ef
kmarks2013 <kmarks2013@gmail.com>
2020-11-27 19:08:30 -0500

    removes height from styled instructions

--- src/components/styles/StyledInstructions.js
+++ src/components/styles/StyledInstructions.js
@@ -10,7 +10,6 @@ export const StyledInstructions = styled.div`
     border: 4px solid #333;
     min-height: 30px;
     width: 100%;
-    height: 50%;
     border-radius: 20px;
     color:  #999;
     background: #000;
