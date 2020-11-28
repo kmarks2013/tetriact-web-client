a22bb549d1e5be8be96113d3779315aa0133dc59
kmarks2013 <kmarks2013@gmail.com>
2020-11-27 20:37:38 -0500

    changes export from default to const to add more button components

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { StyledButton } from './styles/StyledButton'
 
 
-const Button = ({text, callback, width, margin}) => {
+export const Button = ({text, callback, width, margin}) => {
     return (
         <StyledButton width={width} margin={margin} onClick={callback} >
                 {text}
