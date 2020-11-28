296990b2582e78bd4af77063e1546a5fffde6c17
kmarks2013 <kmarks2013@gmail.com>
2020-11-28 00:46:36 -0500

    deletes old styledhigh scores

--- src/components/styles/StyledPopUps.js
+++ src/components/styles/StyledPopUps.js
@@ -10,41 +10,6 @@ export const StyledPopUpWrapper = styled.div`
     z-index: 1;
 `
 
-export const StyledHighScores = styled.div`
-    width: 50%;
-    margin: auto;
-    height: auto;
-    align-content: center;
-    background: #000;
-    opacity: 0.8;
-
-    h2 {
-        font-family: Pixel, Arial, Helvetica, sans-serif;
-        font-size: 24px;
-        margin: auto;
-        padding: 20px;
-        color: white ;
-    }
-
-        h3 {
-            font-family: Pixel, Arial, Helvetica, sans-serif;
-            font-size: 24px;
-            margin: auto;
-            padding: 12px;
-            color: white ;
-        }
-
-    table {
-        ${'' /* margin: auto; */}
-        font-family: Pixel, Arial, Helvetica, sans-serif;
-        font-size: 16px;
-        margin: auto;
-        padding: 0px 20px 20px 20px;
-        color: white;
-        width: 75%;
-    }
-`
-
 export const StyledCloseButton = styled.button`
     box-sizing: border-box;
     align-self: flex-end;
