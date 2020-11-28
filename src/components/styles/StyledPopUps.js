d828345b00e988bbe365198e7b74e092c18e34f2
kmarks2013 <kmarks2013@gmail.com>
2020-11-27 20:01:44 -0500

    removes pasue menu styling from popups

--- src/components/styles/StyledPopUps.js
+++ src/components/styles/StyledPopUps.js
@@ -9,25 +9,6 @@ export const StyledPopUpWrapper = styled.div`
     display: flex;
     z-index: 1;
 `
-
-export const StyledPauseMenu = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: auto;
-    height: auto;
-    margin: auto;
-    background: #000;
-    opacity: 0.8;
-
-    h2 {
-        font-family: Pixel, Arial, Helvetica, sans-serif;
-        font-size: 50px;
-        margin: auto;
-        padding: 40px;
-        color: white ;
-    }
-`
-
 export const StyledGameOverMenu = styled.div`
     width: auto;
     height: auto;
