ba4cc92454fe32d1b872f13c751907835baef521
kmarks2013 <kmarks2013@gmail.com>
2020-11-27 19:25:52 -0500

    adds in styled closed button to pause menu

--- src/components/PauseMenu.js
+++ src/components/PauseMenu.js
@@ -6,6 +6,9 @@ const PauseMenu = ({callback}) => {
     return (
         <StyledPopUpWrapper>
             <StyledPauseMenu >
+            <StyledCloseButton margin={545} onClick={callback}>
+                    X
+            </StyledCloseButton>
                 <h2> Game Paused </h2>
                 <Button text='Resume Game' width={45} margin={120} callback={callback} />
             </StyledPauseMenu>
