01fc8c9e6331daad5c2fcbf0df486d1f4eb0e300
kmarks2013 <kmarks2013@gmail.com>
2020-11-27 19:38:48 -0500

    puts margin back to old version

--- src/components/styles/StyledButton.js
+++ src/components/styles/StyledButton.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 export const StyledButton = styled.button`
     box-sizing: border-box;
     margin: 0 0 13px ${props => props.margin}px ;
-    margin: auto ;
     padding: 20px;
     min-height: 30px;
     width: ${props => props.width}%;
