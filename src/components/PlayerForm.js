7901c88920bc028d3bd4555f50c342d9124005ac
kmarks2013 <kmarks2013@gmail.com>
2020-11-27 20:44:57 -0500

    updates button import to adjust for no default export in Player form

--- src/components/PlayerForm.js
+++ src/components/PlayerForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Button from './Button'
+import {Button} from './Button'
 import { StyledGameOverMenu, StyledCloseButton } from './styles/StyledPopUps'
 
 export const PlayerForm = ({score, newGame, handleSubmit, setGamerTag, gamerTag, closeMenu}) => {
