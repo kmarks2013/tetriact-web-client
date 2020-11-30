527e2e75b6384c3adf820a6a36143a1ac7dd87d8
kmarks2013 <kmarks2013@gmail.com>
2020-11-30 10:28:00 -0500

    removes whitespace form move function

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -130,7 +130,7 @@ const Tetris = () => {
                 movePlayer(1)
             } else if (keyCode === 40){
                 dropPlayer()
-            }  else if (keyCode === 38){
+            } else if (keyCode === 38){
                 playerRotate(stage, 1)
             }
         }
