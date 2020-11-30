bef16594704078c98aaed15143c09b159fb05e36
kmarks2013 <kmarks2013@gmail.com>
2020-11-30 10:48:49 -0500

    adds useEffect to tetris

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import Title from './Title'
 import Footer from './Footer'
 import Display from './Display'
